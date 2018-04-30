44fcbe076f7a5f2cd5dff82c5a3bc6f8ed2bb4d4
Trac Nguyen <nguyendtrac@gmail.com>
2018-04-30 22:47:19 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,9 +7,9 @@ require_relative './course.rb'
 class Scraper
 
   def get_page
- 
+
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
- 
+
     #doc.css(".post").each do |post|
       #course = Course.new
       #course.title = post.css("h2").text
@@ -17,5 +17,11 @@ class Scraper
       #course.description = post.css("p").text
     #end
   end
+
+  def get_courses
+    self.get_page.css(".post")
+  end
+
+  
 
 end
